23289965ec8336fde8b2f4f09153c6e95dee44e5
SebaGPDev <sebakpo120@gmail.com>
2023-02-22 23:20:00 -0300

    new kana

---
+++ CodeWars/JavaScript/Test/Decode_the_Morse_code_1.js
@@ -0,0 +1,60 @@
+const MORSE_CODE = {
+  ".-": "A",
+  "-…": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "…": "S",
+  "-": "T",
+  "..-": "U",
+  "…-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "…--": "3",
+  "….-": "4",
+  "…..": "5",
+  "-….": "6",
+  "--…": "7",
+  "---..": "8",
+  "----.": "9",
+};
+
+const decodeLetter = (letter) => {
+  return MORSE_CODE[letter];
+};
+
+const decodeWord = (word) => {
+  return word.split(" ").map(decodeLetter).join("");
+};
+
+const decodeMorse = (morseCode) => {
+  return morseCode.trim().split("   ").map(decodeWord).join(" ");
+};
+
+// ! Other solution
+
+// decodeMorse = function(morseCode){
+//   return morseCode
+//     .trim()
+//     .split(/  | /)
+//     .map( (code) => MORSE_CODE[code] || ' ')
+//     .join('');
+// }
